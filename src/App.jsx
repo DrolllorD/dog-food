f7d52975f46ca7def9c7374419f9ad5f51a6389c
VadimElagin <elavad7@mail.ru>
2022-12-07 14:57:22 +0700

    create simple project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return <div className="container">
+        <header></header>
+        <main>
+            <h1>Главная страница</h1>
+        </main>
+        <footer></footer>
+    </div>
+}
+export default App;
